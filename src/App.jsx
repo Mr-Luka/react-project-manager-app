7987e4efd0044aa161ff6bf7193aa7ebcca282cc
Luka <luka.luka.luka93@gmail.com>
2025-04-03 12:55:45 -0700

    finishe html, finished css for your projects and buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import './index.css'
+
 function App() {
   return (
     <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+      <div className="projects-wrapper">
+        <div className="add-projects">
+          <h2>YOUR PROJECTS</h2>
+          <button>+ Add Project</button>
+        </div>
+        <div className="projects-form">
+          <div className='cancel-save'>
+            <button>Cancel</button>
+            <button>Save</button>
+          </div>
+          <form>
+            <label>TITLE</label>
+            <input type='text'></input>
+            <label>DESCRIPTION</label>
+            <textarea></textarea>
+            <label>DUE DATE</label>
+            <input type='date'></input>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
