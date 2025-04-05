d3c2901c6fd57a58070aebd67e60dcc6fe73d286
Luka <luka.luka.luka93@gmail.com>
2025-04-04 17:20:49 -0700

    project info done html

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,26 @@ function App() {
           <button>+ Add Project</button>
           <div className='saved-projects'>
             <ol className='ol-saved-projects'>
-
+              
             </ol>
           </div>
+        </div>
+        <div className='project'>
+          <div className='title-and-button'>
+            <h1>TITLE</h1>
+            <button>Delete</button>
+          </div>
+          <p>DATE</p>
+          <p>DESCRIPTIONdsdsdsdsds jhsdjkhsdjkhsds<br/> <br/>Start with the basic</p>
+          <hr></hr>
+          <h2>Tasks</h2>
+          <div className="tasks">
+            <input></input>
+            <button>Add Task</button>
+          </div>
+          <ol>
+            <li>This project does no have any tasks yet.</li>
+          </ol>
         </div>
 
       </div>
