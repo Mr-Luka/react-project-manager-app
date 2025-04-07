dc27a6e040922a956c0f444eb84ad3970bd9f358
Luka <luka.luka.luka93@gmail.com>
2025-04-07 14:35:51 -0700

    adjusted css

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,8 @@ function App() {
           <h2>YOUR PROJECTS</h2>
           <button>+ Add Project</button>
           <div className='saved-projects'>
-            <ol className='ol-saved-projects'></ol>
+            <ol className='ol-saved-projects'>
+            </ol>
           </div>
         </div>
         {newProject ? <AddProjects /> : <NoProjectSelected createProject={handleClick}/>}
