0a6f9f9c7fb16690d4cbbaf7c46a7365f85188ed
Luka <luka.luka.luka93@gmail.com>
2025-04-08 10:58:33 -0700

    fill out form finished

--- src/assets/components/AddProject.jsx
+++ src/assets/components/AddProject.jsx
@@ -1,53 +1,53 @@
 import {useState} from 'react';
 
 export default function AddProject({cancel}){
-  const [form, setForm] = useState({
+  const [formData, setFormData] = useState({
     title: '',
     description: '',
     dueDate: '',
   });
-  const [save, setSave] = useState(false);
 
-  function handleSave(){
-    setSave(save=> !save);
-  }
-
-  function handleInput(inputField, input){
-    setForm(prevForm => {
+  
+  function handleInput(e){
+    const {name, value} = e.target;
+    setFormData(prevForm => {
       return {
         ...prevForm, 
-        [inputField]: input}
-    },
-    console.log(inputField, input))
-  }
-
+        [name]: value}
+      },
+    )}
+    
+    function handleSubmit(e){
+      e.preventDefault();
+      console.log(formData);
+    }
 
 
     return (
         <>
         <div className="projects-form">
-          <form>
-          <div className='cancel-save'>
+          <form onSubmit={handleSubmit}>
+          <div className='cancel-save' >
             <button onClick={cancel}>Cancel</button>
-            <button onClick={handleSave}>Save</button>
+            <button type='submit'>Save</button>
           </div>
             <label>TITLE</label>
             <input 
               id='title'
               name="title"
               type='text' 
-              onChange={(e)=> handleInput(form.title, e.target.value)}></input>
+              onChange={handleInput}></input>
             <label>DESCRIPTION</label>
             <textarea 
               id='description'
               name="description"
-              onChange={(e)=> handleInput(form.description, e.target.value)}></textarea>
+              onChange={handleInput}></textarea>
             <label>DUE DATE</label>
             <input
               id='dueDate'
               name="dueDate"
               type='date' 
-              onChange={(e)=> handleInput(form.dueDate, e.target.value)}></input>
+              onChange={handleInput}></input>
           </form>
         </div>
         </>
