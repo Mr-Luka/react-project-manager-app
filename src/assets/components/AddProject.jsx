d614a3fbbe0a86af45c503b8764e203afa36836b
Luka <luka.luka.luka93@gmail.com>
2025-04-08 10:48:21 -0700

    adjusted html and css

--- src/assets/components/AddProject.jsx
+++ src/assets/components/AddProject.jsx
@@ -1,20 +1,53 @@
 import {useState} from 'react';
+
 export default function AddProject({cancel}){
+  const [form, setForm] = useState({
+    title: '',
+    description: '',
+    dueDate: '',
+  });
+  const [save, setSave] = useState(false);
+
+  function handleSave(){
+    setSave(save=> !save);
+  }
+
+  function handleInput(inputField, input){
+    setForm(prevForm => {
+      return {
+        ...prevForm, 
+        [inputField]: input}
+    },
+    console.log(inputField, input))
+  }
+
+
 
     return (
         <>
         <div className="projects-form">
+          <form>
           <div className='cancel-save'>
             <button onClick={cancel}>Cancel</button>
-            <button>Save</button>
+            <button onClick={handleSave}>Save</button>
           </div>
-          <form>
             <label>TITLE</label>
-            <input type='text'></input>
+            <input 
+              id='title'
+              name="title"
+              type='text' 
+              onChange={(e)=> handleInput(form.title, e.target.value)}></input>
             <label>DESCRIPTION</label>
-            <textarea></textarea>
+            <textarea 
+              id='description'
+              name="description"
+              onChange={(e)=> handleInput(form.description, e.target.value)}></textarea>
             <label>DUE DATE</label>
-            <input type='date'></input>
+            <input
+              id='dueDate'
+              name="dueDate"
+              type='date' 
+              onChange={(e)=> handleInput(form.dueDate, e.target.value)}></input>
           </form>
         </div>
         </>
