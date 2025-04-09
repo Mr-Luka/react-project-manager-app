1e274d598d2336369206cf987f4bb096cdb811f5
Luka <luka.luka.luka93@gmail.com>
2025-04-09 15:10:25 -0700

    finished with project, added handle input, handleSubmit and clear task button

--- src/assets/components/Project.jsx
+++ src/assets/components/Project.jsx
@@ -1,10 +1,30 @@
-
+import {useState} from 'react';
 import { convertDate } from '../utils/convertDate.js';
 
 export default function Project({title, date, description, remove}){
+  const [newTask, setNewTask] = useState('');
+  const [newTasks, setNewTasks] = useState([]);
+  const [deletingTask, setDeletingTask] = useState([]);
 
   const formattedDate = convertDate(date);
 
+  function handleInput(e){
+    setNewTask(e.target.value);
+  }
+
+  function handleSubmit(e){
+  e.preventDefault();
+  if(newTask.trim() !== ''){
+    setNewTasks(prevTask=> [...prevTask, newTask]); 
+    setNewTask('');
+  }
+}
+
+function clearTask(taskIndex){
+  const taskObject = newTasks[taskIndex];
+  const filteredTasks = newTasks.filter(project => project !== taskObject)
+  setNewTasks(filteredTasks);
+}
 
     return (
         <div className='project'>
@@ -16,13 +36,17 @@ export default function Project({title, date, description, remove}){
           <p>{description}</p>
           <hr></hr>
           <h2>Tasks</h2>
-          <div className="tasks">
-            <input></input>
-            <button>Add Task</button>
+          <form onSubmit={handleSubmit}>
+            <div className="tasks">
+            <input onChange={handleInput} value={newTask}></input>
+            <button type='submit'>Add Task</button>
           </div>
-          <ol>
-            <li>This project does no have any tasks yet.</li>
+          <ol className={`tasks-ol ${newTasks.length > 0 ? 'active-tasks' : ''}`}>
+            {newTasks.length > 0 ? (newTasks.map((task, taskIndex) => (
+      <li key={taskIndex}><p>{task}</p><button onClick={()=> clearTask(taskIndex)}>Clear</button></li>
+    ))) : <li>This project does no have any tasks yet.</li>}
           </ol>
+          </form>
         </div>
     )
 }
